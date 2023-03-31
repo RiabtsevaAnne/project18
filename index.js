e0b0d8ceb66d8b59b1dceae5145598c88997ddca
RiabtsevaAnne <hannariabtseva05@gmail.com>
2023-03-31 14:56:16 +0300

    project18

---
+++ index.js
@@ -0,0 +1,12 @@
+if ($("#list").css("display") === "none") {
+     $("#list").slideDown();
+ } else {
+      $("#list").slideUp();
+    }
+
+$(function btn() {
+	$('#list').fadeIn(2000);
+})
+$(function btn() {
+	$('#list').fadeOut(3000);
+})
